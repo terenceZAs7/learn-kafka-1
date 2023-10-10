b7f8621e796879c765f35b509e5f0a986a4a5ebf
Terence Palani <terence@synthesis.co.za>
2023-10-10 10:55:10 +0200

    Add kafka producer.

--- kafka-producer-application/src/main/java/io/confluent/developer/KafkaProducerApplication.java
+++ kafka-producer-application/src/main/java/io/confluent/developer/KafkaProducerApplication.java
@@ -1,6 +1,5 @@
 package io.confluent.developer;
 
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -19,83 +18,83 @@ import java.util.stream.Collectors;
 
 public class KafkaProducerApplication {
 
-    private final Producer<String, String> producer;
-    final String outTopic;
+  private final Producer<String, String> producer;
+  final String outTopic;
 
-    public KafkaProducerApplication(final Producer<String, String> producer,
-                                    final String topic) {
-        this.producer = producer;
-        outTopic = topic;
+  public KafkaProducerApplication(final Producer<String, String> producer,
+      final String topic) {
+    this.producer = producer;
+    outTopic = topic;
+  }
+
+  public Future<RecordMetadata> produce(final String message) {
+    final String[] parts = message.split("-");
+    final String key, value;
+    if (parts.length > 1) {
+      key = parts[0];
+      value = parts[1];
+    } else {
+      key = null;
+      value = parts[0];
     }
+    final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outTopic, key, value);
+    return producer.send(producerRecord);
+  }
 
-    public Future<RecordMetadata> produce(final String message) {
-        final String[] parts = message.split("-");
-        final String key, value;
-        if (parts.length > 1) {
-            key = parts[0];
-            value = parts[1];
-        } else {
-            key = null;
-            value = parts[0];
+  public void shutdown() {
+    producer.close();
+  }
+
+  public static Properties loadProperties(String fileName) throws IOException {
+    final Properties envProps = new Properties();
+    final FileInputStream input = new FileInputStream(fileName);
+    envProps.load(input);
+    input.close();
+
+    return envProps;
+  }
+
+  public void printMetadata(final Collection<Future<RecordMetadata>> metadata,
+      final String fileName) {
+    System.out.println("Offsets and timestamps committed in batch from " + fileName);
+    metadata.forEach(m -> {
+      try {
+        final RecordMetadata recordMetadata = m.get();
+        System.out.println(
+            "Record written to offset " + recordMetadata.offset() + " timestamp " + recordMetadata.timestamp());
+      } catch (InterruptedException | ExecutionException e) {
+        if (e instanceof InterruptedException) {
+          Thread.currentThread().interrupt();
         }
-        final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outTopic, key, value);
-        return producer.send(producerRecord);
+      }
+    });
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 2) {
+      throw new IllegalArgumentException(
+          "This program takes two arguments: the path to an environment configuration file and" +
+              "the path to the file with records to send");
     }
 
-    public void shutdown() {
-        producer.close();
+    final Properties props = KafkaProducerApplication.loadProperties(args[0]);
+    final String topic = props.getProperty("output.topic.name");
+    final Producer<String, String> producer = new KafkaProducer<>(props);
+    final KafkaProducerApplication producerApp = new KafkaProducerApplication(producer, topic);
+
+    String filePath = args[1];
+    try {
+      List<String> linesToProduce = Files.readAllLines(Paths.get(filePath));
+      List<Future<RecordMetadata>> metadata = linesToProduce.stream()
+          .filter(l -> !l.trim().isEmpty())
+          .map(producerApp::produce)
+          .collect(Collectors.toList());
+      producerApp.printMetadata(metadata, filePath);
+
+    } catch (IOException e) {
+      System.err.printf("Error reading file %s due to %s %n", filePath, e);
+    } finally {
+      producerApp.shutdown();
     }
-
-    public static Properties loadProperties(String fileName) throws IOException {
-        final Properties envProps = new Properties();
-        final FileInputStream input = new FileInputStream(fileName);
-        envProps.load(input);
-        input.close();
-
-        return envProps;
-    }
-
-    public void printMetadata(final Collection<Future<RecordMetadata>> metadata,
-                              final String fileName) {
-        System.out.println("Offsets and timestamps committed in batch from " + fileName);
-        metadata.forEach(m -> {
-            try {
-                final RecordMetadata recordMetadata = m.get();
-                System.out.println("Record written to offset " + recordMetadata.offset() + " timestamp " + recordMetadata.timestamp());
-            } catch (InterruptedException | ExecutionException e) {
-                if (e instanceof InterruptedException) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        });
-    }
-
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            throw new IllegalArgumentException(
-                    "This program takes two arguments: the path to an environment configuration file and" +
-                            "the path to the file with records to send");
-        }
-
-        final Properties props = KafkaProducerApplication.loadProperties(args[0]);
-        final String topic = props.getProperty("output.topic.name");
-        final Producer<String, String> producer = new KafkaProducer<>(props);
-        final KafkaProducerApplication producerApp = new KafkaProducerApplication(producer, topic);
-
-        String filePath = args[1];
-        try {
-            List<String> linesToProduce = Files.readAllLines(Paths.get(filePath));
-            List<Future<RecordMetadata>> metadata = linesToProduce.stream()
-                    .filter(l -> !l.trim().isEmpty())
-                    .map(producerApp::produce)
-                    .collect(Collectors.toList());
-            producerApp.printMetadata(metadata, filePath);
-
-        } catch (IOException e) {
-            System.err.printf("Error reading file %s due to %s %n", filePath, e);
-        }
-        finally {
-            producerApp.shutdown();
-        }
-    }
+  }
 }
